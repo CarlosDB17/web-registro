03dea884b2b74d574fe3023a6577731d6f0460fb
Carlos DB <carlosdbtic@gmail.com>
2025-03-20 13:00:31 +0100

    Primeros componentes añadidos

--- app-angular-registro/src/app/app.component.ts
+++ app-angular-registro/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ListadoUsuariosComponent } from './components/listado-usuarios/listado-usuarios.component';
+import { ConsultasComponent } from './components/consultas/consultas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistroComponent, ListadoUsuariosComponent, ConsultasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
