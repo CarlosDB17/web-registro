07b6afc5659dcc23b93c152357d028c9010f6f7a
Carlos DB <carlosdbtic@gmail.com>
2025-03-27 10:00:56 +0100

    Añadir configuración de Firebase en Dockerfile y actualizar importaciones en app.config.ts

--- app-angular-registro/src/app/app.config.ts
+++ app-angular-registro/src/app/app.config.ts
@@ -1,18 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { firebaseConfig } from '../../firebase.config'; // Importa la configuración
 
+// importa la configuracion desde environments
+import { firebaseConfig } from '../environments/firebase.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"pf25-carlos-db","appId":"1:302016834907:web:5738505c9a6f5fd8cb7e7e","storageBucket":"pf25-carlos-db.firebasestorage.app","apiKey":"AIzaSyC6y_F7hO3u7FFOODPaT-m6WqK3DExyxTQ","authDomain":"pf25-carlos-db.firebaseapp.com","messagingSenderId":"302016834907","measurementId":"G-E85ZY92GXM"})), provideAuth(() => getAuth()) 
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)), 
+    provideAuth(() => getAuth()),
   ]
 };
