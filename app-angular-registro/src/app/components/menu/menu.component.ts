6bee9fd4b33f88f93908f150451613e8856688aa
Carlos DB <carlosdbtic@gmail.com>
2025-04-02 12:43:59 +0200

    Agregar lógica para resaltar el botón activo en el menú de navegación y mejorar la gestión de rutas

--- app-angular-registro/src/app/components/menu/menu.component.ts
+++ app-angular-registro/src/app/components/menu/menu.component.ts
@@ -1,24 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth-services/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
-    private authservice = inject(AuthService);
-    private router = inject(Router);
+  private authservice = inject(AuthService);
+  private router = inject(Router);
+  currentRoute: string = ''; // variable para almacenar la ruta actual
+
+  ngOnInit(): void {
+    this.currentRoute = this.router.url; // ruta inicial
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url; // actualiza la ruta cuando cambia
+      }
+    });
+  }
 
   logOut(): void {
     this.authservice.logOut()
       .then(() => {
         console.log('Sesión cerrada');
-        this.router.navigate(['/auth/log-in']); // redirige al usuario a la página de inicio de sesión
+        this.router.navigate(['/auth/log-in']); // redirige al usuario a la página de inicio de sesion
       })
       .catch((error) => {
         console.error('Error al cerrar sesión:', error);
@@ -26,6 +38,6 @@ export class MenuComponent {
   }
 
   navigateTo(route: string): void {
-    this.router.navigate([route]); // metodo para manejar la navegación
+    this.router.navigate([route]); // metodo para manejar la navegacion
   }
 }
